61d68638af7d031c0533784427c469f95ed6723c
Lucas Espindola <lucas.espindola@hotmail.com>
2019-11-16 00:36:47 -0300

    Updates

---
+++ precache-manifest.adc8228cf8f7a53463be7a9312538a28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ba2da5a380cba443dd2cb8a6c32b9d",
+    "url": "/cache/index.html"
+  },
+  {
+    "revision": "c47c58806c5e7f3039c0",
+    "url": "/cache/static/css/main.daf0f28a.chunk.css"
+  },
+  {
+    "revision": "b8a041db104d3cab6c68",
+    "url": "/cache/static/js/2.10417b39.chunk.js"
+  },
+  {
+    "revision": "c47c58806c5e7f3039c0",
+    "url": "/cache/static/js/main.b7e9da01.chunk.js"
+  },
+  {
+    "revision": "4890bb85b3190fcd2a5b",
+    "url": "/cache/static/js/runtime-main.df176acf.js"
+  }
+]);
