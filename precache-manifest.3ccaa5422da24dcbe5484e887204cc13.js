199ad4b072389f271e43ec8d23b1683269dc1850
Lucas Espíndola <lucas.espindola@hotmail.com>
2019-11-20 13:34:31 -0300

    Updates

---
+++ precache-manifest.3ccaa5422da24dcbe5484e887204cc13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917b4d5ef02c478fe8d7eed4d05d5b7e",
+    "url": "/cache/index.html"
+  },
+  {
+    "revision": "276d04a52249d5bb91f9",
+    "url": "/cache/static/css/main.824a9058.chunk.css"
+  },
+  {
+    "revision": "d77a67a2087152a6e577",
+    "url": "/cache/static/js/2.5732cfe1.chunk.js"
+  },
+  {
+    "revision": "276d04a52249d5bb91f9",
+    "url": "/cache/static/js/main.9d1fd5a5.chunk.js"
+  },
+  {
+    "revision": "4890bb85b3190fcd2a5b",
+    "url": "/cache/static/js/runtime-main.df176acf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cache/precache-manifest.b962232de95379364bafd6dc5f29064b.js"
+  "/cache/precache-manifest.3ccaa5422da24dcbe5484e887204cc13.js"
 );
 
 self.addEventListener('message', (event) => {
